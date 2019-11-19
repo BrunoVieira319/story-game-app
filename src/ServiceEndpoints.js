0a96938e1b8f7f96abfa166c8fee53ea2fb65f7f
BrunoVieira319 <brunovieira319@hotmail.com>
2019-11-18 22:57:34 -0300

    change endpoint

--- src/ServiceEndpoints.js
+++ src/ServiceEndpoints.js
@@ -1,6 +1,6 @@
 const ServicesEndpoints = {
-    USER_SERVICE: "http://34.69.112.79:8082/v1",
+    USER_SERVICE: "http://34.70.115.126:8082/v1",
     STORY_SERVICE: "http://34.66.162.191:8084/v1",
-    ACT_SERVICE: "http://104.155.169.57:8086/v1"
+    ACT_SERVICE: "http://104.197.170.238:8086/v1"
 };
 export default Object.freeze(ServicesEndpoints)
